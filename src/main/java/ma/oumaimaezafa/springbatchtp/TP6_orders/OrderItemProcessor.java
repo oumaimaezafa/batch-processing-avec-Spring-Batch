6439f328c0ac247636e04c3ec715816ca85b047c
Oumaima Ez <oumaimaezafa12@gmail.com>
2025-01-11 01:36:23 +0100

    add tp6

--- src/main/java/ma/oumaimaezafa/springbatchtp/TP6_orders/OrderItemProcessor.java
+++ src/main/java/ma/oumaimaezafa/springbatchtp/TP6_orders/OrderItemProcessor.java
@@ -1,13 +1,16 @@
 package ma.oumaimaezafa.springbatchtp.TP6_orders;
-
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class OrderItemProcessor implements ItemProcessor<Order, Order> {
 
     @Override
     public Order process(Order order) throws Exception {
+        System.out.println("Données originales : " + order);
+
         double discountedAmount = order.amount() * 0.9;
-        return new Order(order.orderId(), order.customerName(), discountedAmount);
+        Order discountedOrder = new Order(order.orderId(), order.customerName(), discountedAmount);
+        return discountedOrder;
     }
 }
